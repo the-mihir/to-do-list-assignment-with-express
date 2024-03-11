60eb4e4203cd0015c5329ed3d408af12de13087b
Mihir Das <mihirkantho@gmail.com>
2024-03-11 19:37:31 +0600

    user Registration and login is done

--- src/controller/UserController.js
+++ src/controller/UserController.js
@@ -1,10 +1,57 @@
-exports.registration= (req,res)=>{
+
+const UserModel = require("../model/UsersModel");
+const jwt = require("jsonwebtoken");
+const sendEmail = require("../utility/SendEmail");
+const OTPModel = require("../model/OTPModel");
+
+
+exports.registration= async (req,res)=>{
+    try{
+        let reqBody = req.body;
+        await UserModel.create(reqBody);
+        res.json({
+            status: 200,
+            message: "Registration Successful"
+        })
+    } catch (error) {
+        res.json({
+            status: 500,
+            message: error.message
+        })
+    }
 
 }
 
-exports.login= (req,res)=>{
+exports.login = async (req, res) => {
+    try {
+        let reqBody = req.body;
+        let user = await UserModel.findOne({ email: reqBody.email });
 
-}
+        if (user) {
+            let Payload = {
+                exp: Math.floor(Date.now() / 1000) + (24 * 60 * 60),
+                data: reqBody.email
+            };
+            let token = jwt.sign(Payload, "secretKey");
+            res.json({
+                status: 200,
+                message: "Login Successful",
+                token: token
+            });
+        } else {
+            res.json({
+                status: 500,
+                message: "User Not Found"
+            });
+        }
+    } catch (error) {
+        res.json({
+            status: 500,
+            message: error.message
+        });
+    }
+};
+
 
 exports.profileDetails= (req,res)=>{
 
